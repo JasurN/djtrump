68658f0a3d20f87f7550e87a9ac04816183d8da6
jasurn <97jasur@gmail.com>
2020-02-19 16:27:52 +0500

    to check slack integration

--- djtrump/tests/test_sample.py
+++ djtrump/tests/test_sample.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 
 class SampleTestCase(TestCase):
     def test_two_plus_two(self):
-        self.assertEqual(2+2, 4)
+        self.assertEqual(2+1, 3)
 
     def test_nine_minus_three(self):
         self.assertEqual(9-3, 6)
